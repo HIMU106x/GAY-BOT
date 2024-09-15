d3082dbba0e09b1d2027021aa2c1ca911e0a0a0e
𝄟✮⃝❤𝗛𝗜×͜×𝗠𝗨✮⃝❤𝄟 <166631945+HIMU999FF@users.noreply.github.com>
2024-09-15 22:06:30 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -43,8 +43,8 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
       case "message_reaction":
         onReaction();
 
-                if(event.reaction == "🖕"){
-  if(event.userID == "100072881080249"){
+                if(event.reaction == "😠"){
+  if(event.userID == "100066839859875"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -53,7 +53,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send("")
   }
   }
-        if(event.reaction == "😠"){
+        if(event.reaction == "☠️"){
   if(event.senderID == api.getCurrentUserID()){if(event.userID == "100072881080249"){
     message.unsend(event.messageID)
 }else{
